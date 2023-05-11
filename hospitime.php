4f42a65c364c8c8da58ce968cfb0c1d9717fc6d4
mohamedkante <114979734+mohamedkante@users.noreply.github.com>
2023-05-11 19:59:56 +0200

    page d'accueil

--- hospitime.php
+++ hospitime.php
@@ -2,35 +2,56 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Titre de la page</title>
+    <title>HospiTime le specialiste des rendez-vous</title>
     <!-- Liens vers les fichiers CSS et JS -->
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
  </head>
    
 <body>
+
  <header> 
-    
     <div class="navbar">
-       <!-- <a href="/" title="Accueil" aria-label="Accueil" class="flex"><div class="logo-doctolib logo-doctolib-white"></div> </a>
-
-        <a href="#">À propos</a> -->
-        <!-- <a href="#">Services</a> -->
+     <ul>
+        <a href="#" class="register-link"> S'inscrire</a>
         <a href="formulaire.php#"> 👤 Se connecter </a>
-        <div class="logo">
+        <a href="#"> Hopitaux</a> 
+    <!-- </div> -->
+    </ul>
+    <div class="logo">
         <img src="doc.png" alt=" ">
-    </div>
-    </div> 
-
+     </div>
  </header>
-  <?php 
+
+  <main> 
+    <div class="search-bar">
+        <input type="text" class="search-input" placeholder=" 🔍 Nom, établissement, spécialisté...">
+        <button class="search-button"> ▷</button>
+    </div> 
+ </main>
+
+     <?php 
   // Inclusion du fichier connexion_bdd.php pour établir la connexion à la base de données
        include("includes.php");
       ?>
-        
-	 
+      <? $terme = $_GET['recherche'];
+         $sql = "SELECT * FROM medecin WHERE colonne LIKE '%$terme%'";
+         $result = mysqli_query($conn, $sql);
+      ?>
     <!-- Pied de page -->
- <footer>
+  <footer> 
+     <!-- Votre contenu de footer ici -->
+
+     <div class="download-section">
+       <a href="https://www.apple.com/app-store/" target="_blank">
+       <img src="applApps.png" title="Application mobile dans l'App Store d'Apple" alt="Application mobile dans l'App Store d'Apple" alt="Télécharger sur App Store">
+       </a>
+       <a href="https://play.google.com/store" target="_blank">
+         <img src="androidApps.png" alt="Disponible sur Android">
+      </a>
+   </div>       <!-- <a href="https://play.google.com/store" target="_blank">
+         <img src="androidApps.png" alt="Disponible sur Android">
+       </a>  -->
         <!-- <p>Copyright © 2023</p> -->
   </footer>
 </body>
